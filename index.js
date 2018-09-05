a00b3c28f9d67e7f6b5614c91630582af5ca53a8
Micky Hulse <mickyhulse@gmail.com>
2018-09-05 12:47:54 -0700

    SAVEPOINT

--- index.js
+++ index.js
@@ -18,7 +18,17 @@ glob.sync('**/*.md', { cwd: `./wiki` }).forEach((file, i) => {
 
     data.push('\n');
 
-    data.push(toc(fs.readFileSync('./wiki/' + file, 'utf8')).content);
+    data.push(
+      toc(fs.readFileSync('./wiki/' + file, 'utf8'), {
+        linkify: function(tok, text, slug, options) {
+          // update tok.content to how you want it
+          tok.content = `[${text}](../../wiki/${title}#${slug})`;
+          return tok;
+        }
+      }).content
+    );
+
+    data.push('\n');
 
   }
 
