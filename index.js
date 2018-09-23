f682b445731584b621f237c5ffad18f7341f48e8
Micky Hulse <mickyhulse@gmail.com>
2018-09-23 11:21:41 -0700

    SAVEPOINT

--- index.js
+++ index.js
@@ -7,35 +7,56 @@ const toc = require('markdown-toc');
 const data = [];
 const top = '[<img width="32" height="32" align="right" src="https://assets-cdn.github.com/images/icons/emoji/unicode/261d.png" class="emoji" title="TOC">](#readme)';
 
-data.push(`
-## External Links&nbsp;${top}
-- My Blender YouTube channel: [TL;DR – 3D](https://www.youtube.com/channel/UCFoE0SgDe0t82Ra2UAit57Q)
-`)
+//------------------------------------------------------------------------------
+// YouTube cruft …
+// @TODO Clean up code; make modular and get rid of `next()` jenk!
 
-glob.sync('**/*.md', { cwd: `./wiki` }).forEach((file, i) => {
+const ypi = require('youtube-channel-videos');
+const credentials = require('./credentials.json');
+const _ = require('underscore');
 
-  let title = file.replace('.md', '');
+data.push(`## My Blender YouTube channel: [TL;DR – 3D](https://www.youtube.com/channel/UCFoE0SgDe0t82Ra2UAit57Q)${top}`)
+data.push('\n');
 
-  if (title != 'Home') {
-
-    console.log(title);
-
-    data.push(`## [${title.replace(/-/g, ' ')}](../../wiki/${title.replace(/\s/g, '-')})&nbsp;${top}`);
-
-    data.push(
-      toc(fs.readFileSync('./wiki/' + file, 'utf8'), {
-        linkify: function(tok, text, slug, options) {
-          // update tok.content to how you want it
-          tok.content = `[${text}](../../wiki/${title}#${slug})`;
-          return tok;
-        }
-      }).content
-    );
-
-    data.push('\n');
-
-  }
-
+ypi.channelVideos(credentials.api_key, 'UCFoE0SgDe0t82Ra2UAit57Q', function(videos) {
+  _(videos).each(video => {
+    data.push(`- [${video.snippet.title}](https://www.youtube.com/watch?v=${video.id.videoId})`)
+  });
+  data.push('\n');
+  // Stupid, but I don’t have much time to make this better:
+  next();
 });
 
-fs.writeFileSync('README.md', data.join('\n'));
+//------------------------------------------------------------------------------
+
+function next() {
+
+  glob.sync('**/*.md', { cwd: `./wiki` }).forEach((file, i) => {
+
+    let title = file.replace('.md', '');
+
+    if (title != 'Home') {
+
+      console.log(title);
+
+      data.push(`## [${title.replace(/-/g, ' ')}](../../wiki/${title.replace(/\s/g, '-')})&nbsp;${top}`);
+
+      data.push(
+        toc(fs.readFileSync('./wiki/' + file, 'utf8'), {
+          linkify: function(tok, text, slug, options) {
+            // update tok.content to how you want it
+            tok.content = `[${text}](../../wiki/${title}#${slug})`;
+            return tok;
+          }
+        }).content
+      );
+
+      data.push('\n');
+
+    }
+
+  });
+
+  fs.writeFileSync('README.md', data.join('\n'));
+
+}
